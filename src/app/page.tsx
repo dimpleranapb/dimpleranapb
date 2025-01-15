d663a3911c837be37eadc683b882db06674a9b42
Dimple Rana <dimpleranaji@gmail.com>
2025-01-15 17:50:39 +0530

    Set font and add Header Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { HeroSection } from "@/sections/Hero";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
+      <HeroSection />
     </div>
   );
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,42 @@
+import memojiImage from "@/assets/images/memoji-computer.png";
+import Image from "next/image";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className="py-32">
+      <div className="container">
+        <div className="flex flex-col items-center ">
+          <Image
+            src={memojiImage}
+            className="size-[100px]"
+            alt="Memoji of me sitting at a computer"
+          />
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-medium">
+              Available for new projects
+            </div>
+          </div>
+        </div>
+
+        <h1>Building Exceptional User Experiences</h1>
+        <p>
+          I'm a software engineer who specializes in building exceptional user
+          experiences. I'm passionate about creating products that are both
+          beautiful and functional.
+        </p>
+        <div>
+          <button>
+            <span>Explore My Work</span>
+            <ArrowDown />
+          </button>
+          <button>
+            <span>👋</span>
+            <span>Let's connect</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
