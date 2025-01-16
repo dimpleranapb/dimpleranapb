0309f085ca2d50566fd96972e3e3666408d8c2fd
Dimple Rana <dimpleranaji@gmail.com>
2025-01-16 23:53:03 +0530

    Creating Hero Section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,27 @@
 import memojiImage from "@/assets/images/memoji-computer.png";
 import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
-
+import grainImage from "@/assets/images/grain.jpg";
+import StarIcon from "@/assets/icons/star.svg";
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+      <div
+        className="absolute inset-0 -z-30 opacity-5"
+        style={{ backgroundImage: `url(${grainImage.src})` }}
+      ></div>
+      <div className="size-[620px] hero-ring"></div>
+      <div className="size-[820px] hero-ring"></div>
+      <div className="size-[1020px] hero-ring"></div>
+      <div className="size-[1220px] hero-ring"></div>
+      <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 border">
+        <div className=" border border-red-500 size-[800px] animate-spin [animation-duration:30s]">
+          <div className="border border-red-500 inline-flex animate-spin [animation-duration:5s] ">
+            <StarIcon className="size-28 text-emerald-300" />
+          </div>
+        </div>
+      </div>
+
       <div className="container">
         <div className="flex flex-col items-center ">
           <Image
@@ -19,21 +36,24 @@ export const HeroSection = () => {
             </div>
           </div>
         </div>
-
-        <h1>Building Exceptional User Experiences</h1>
-        <p>
-          I'm a software engineer who specializes in building exceptional user
-          experiences. I'm passionate about creating products that are both
-          beautiful and functional.
-        </p>
-        <div>
-          <button>
-            <span>Explore My Work</span>
-            <ArrowDown />
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Building Exceptional User Experiences
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            I'm a software engineer who specializes in building exceptional user
+            experiences. I'm passionate about creating products that are both
+            beautiful and functional.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+            <span className="font-semibold">Explore My Work</span>
+            <ArrowDown className="size-4" />
           </button>
-          <button>
+          <button className="inline-flex items-center gap-2 border bg-white px-6 h-12 rounded-xl mt-4 text-gray-900">
             <span>👋</span>
-            <span>Let's connect</span>
+            <span className="font-semibold">Let's connect</span>
           </button>
         </div>
       </div>
