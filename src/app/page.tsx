d988bbf248d9c3bc60cd45182cde9657362c584e
Dimple Rana <dimpleranaji@gmail.com>
2025-01-21 16:37:22 +0530

    Add Tape Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
+import { TapeSection } from "@/sections/Tape";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <ProjectsSection />
+      <TapeSection />
     </div>
   );
 }
--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,29 @@
+const words = [
+  "Performant",
+  "Accessible",
+  "Reliable",
+  "Secure",
+  "Scalable",
+  "Responsive",
+];
+import StarIcon from "@/assets/icons/star.svg";
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="py-16 lg:py-24 overflow-x-clip">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400  -rotate-3 -mx-1">
+       <div className="flex [mask-image:liner-gradient(to_right, transparent,black_10%,black_90%,transparent -mx-3)]">
+        <div className="flex flex-none gap-4 py-3">
+          <div className="flex gap-4 items-center">
+            {words.map((word) => (
+              <div key={word}>
+                <span className="text-gray-900 uppercase font-extrabold text-sm">{word}</span>
+                <StarIcon classname="size-6 text-gray-900 -rotate-12" />
+              </div>
+            ))}
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
 };
