1b5e0f2f15e932894c0c69f6eba748bc5a7d9edd
Dimple Rana <dimpleranaji@gmail.com>
2025-02-22 22:01:27 +0530

    minor changes

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import ArrowUprightIcon from "@/assets/icons/arrow-up-right.svg";
 import grainImage from "@/assets/images/grain.jpg";
+
 export const ContactSection = () => {
   return (
-    <div className="py-16 pt-12 lg:py-24 lg:pt-20 " id="contact">
+    <div className="py-16 pt-12 lg:py-24 lg:pt-20" id="contact">
       <div className="container">
         <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
           <div
@@ -22,10 +24,12 @@ export const ContactSection = () => {
               </p>
             </div>
             <div>
-              <button className="text-white bg-gray-900 inline-flex justify-center items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
-                <span className="font-semibold"> Contact Me</span>
-                <ArrowUprightIcon className="size-4" />
-              </button>
+              <Link href="/contact">
+                <button className="text-white bg-gray-900 inline-flex justify-center items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
+                  <span className="font-semibold">Contact Me</span>
+                  <ArrowUprightIcon className="size-4" />
+                </button>
+              </Link>
             </div>
           </div>
         </div>
